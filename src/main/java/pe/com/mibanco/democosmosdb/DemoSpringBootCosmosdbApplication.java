ae573c2d45aa4529bfdbc0dfc22a47c2b6d692ff
Branny Gonzales <branny.gonzales@avantica.com>
2021-01-20 16:43:36 -0500

    Para WAR

--- src/main/java/pe/com/mibanco/democosmosdb/DemoSpringBootCosmosdbApplication.java
+++ src/main/java/pe/com/mibanco/democosmosdb/DemoSpringBootCosmosdbApplication.java
@@ -2,10 +2,9 @@ package pe.com.mibanco.democosmosdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoSpringBootCosmosdbApplication extends SpringBootServletInitializer {
+public class DemoSpringBootCosmosdbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootCosmosdbApplication.class, args);
