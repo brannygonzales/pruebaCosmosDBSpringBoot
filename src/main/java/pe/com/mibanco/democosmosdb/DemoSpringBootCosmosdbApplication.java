97c9e62c8db251faa57bd63ed249c4a97f0dbdc0
Branny Gonzales <branny.gonzales@avantica.com>
2021-01-20 16:25:45 -0500

    Cambio para servlet

--- src/main/java/pe/com/mibanco/democosmosdb/DemoSpringBootCosmosdbApplication.java
+++ src/main/java/pe/com/mibanco/democosmosdb/DemoSpringBootCosmosdbApplication.java
@@ -2,9 +2,10 @@ package pe.com.mibanco.democosmosdb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoSpringBootCosmosdbApplication {
+public class DemoSpringBootCosmosdbApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringBootCosmosdbApplication.class, args);
